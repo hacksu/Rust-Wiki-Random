c567263804fbcdffa9a90400328528c8b047fc82
Isaac Park <isaac.a.park@gmail.com>
2016-09-13 17:37:08 -0400

    Remove Code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,2 @@
-use std::io;
-
-
-     fn main() {
-         let mut i = String::new();
-         i = match io::stdin().read_line(&mut i) {
-             Ok(_) => i.trim().to_string(),
-             _ => "".to_string()
-         };
-         println!("Hello {}!", i);
-     }
+fn main() {
+}
